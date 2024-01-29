30236d1bb3a94acb614970a56c55b194e9dbb88f
pavan <pavan@gmail.com>
2024-01-29 12:30:51 +0530

    modified in vowelscript

--- vowelscript2.sh
+++ vowelscript2.sh
@@ -9,7 +9,10 @@ read char
 
 case $char in
 	[aeiou])
-	echo"the character is a lower case vowel"
+	echo "the character is a lower case vowel"
+	;;
+	[AEIOU])
+	echo "the character is a upper case vowel"
 	;;
 *)
 	echo "The cahracter is not lovercase vowel"
