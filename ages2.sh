530982e066efb465e856f422f1b0762b1ce99f81
pavan <pavan@gmail.com>
2024-01-29 13:05:09 +0530

    corrected

--- ages2.sh
+++ ages2.sh
@@ -8,7 +8,7 @@ read age
 if [ $age -lt 13 ]
 then
 	echo "Display as a child"
-elif [ $age -gt 13 -a $age -lt 19 ]
+elif [ $age -ge 13 -a $age -lt 19 ]
 then
 	echo "Display as a teenager"
 elif [ $age -gt 20 ]
